f08ae4f1f17137a7ea51948060b843ed94f8e705
Ajmain Nishu <ajmainnishu@gmail.com>
2023-10-19 12:43:34 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.5ffrq.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const toysCollection = client.db('usersDB').collection('toys');
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Cudly Toy Store Running')
+})
+
+app.listen(port, () => {
+    console.log(`Cudly Toy Store Running ${port}`)
+})
