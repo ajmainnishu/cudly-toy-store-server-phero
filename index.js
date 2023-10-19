52acdd113b06c483cabee557c75dbc473ffe60d5
Ajmain Nishu <ajmainnishu@gmail.com>
2023-10-19 18:52:33 +0600

    feat: Single toy get added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 app.use(cors());
@@ -25,6 +25,13 @@ async function run() {
         // all toys
         app.get('/toys', async (req, res) => {
             const result = await toysCollection.find().toArray();
+            res.send(result);
+        })
+        // single toy
+        app.get('/toy/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await toysCollection.findOne(query);
             res.send(result);
         })
         await client.db("admin").command({ ping: 1 });
